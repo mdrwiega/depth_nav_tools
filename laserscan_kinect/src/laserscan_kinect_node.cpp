2ca47bba1349a5e62f02b83ef228eb78b978d284
Michal Drwiega <drwiega.michal@gmail.com>
2020-05-03 03:04:19 +0200

    [laserscan_kinect] Code cleanup

--- laserscan_kinect/src/laserscan_kinect_node.cpp
+++ laserscan_kinect/src/laserscan_kinect_node.cpp
@@ -1,44 +1,8 @@
-/******************************************************************************
- * Software License Agreement (BSD License)
- *
- * Copyright (c) 2016, Michal Drwiega (drwiega.michal@gmail.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
- *     3. Neither the name of the copyright holder nor the names of its
- *        contributors may be used to endorse or promote products derived
- *        from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *****************************************************************************/
-/**
- * @file   laserscan_kinect_node.cpp
- * @author Michal Drwiega (drwiega.michal@gmail.com)
- * @brief  laserscan_kinect package
- */
-
 #include <laserscan_kinect/laserscan_kinect_node.h>
 #include <boost/bind.hpp>
 
 namespace laserscan_kinect {
 
-//=================================================================================================
 LaserScanKinectNode::LaserScanKinectNode(ros::NodeHandle& n, ros::NodeHandle& pnh) :
     pnh_(pnh), it_(n), srv_(pnh)
 {
@@ -53,13 +17,11 @@ LaserScanKinectNode::LaserScanKinectNode(ros::NodeHandle& n, ros::NodeHandle& pn
                                                boost::bind(&LaserScanKinectNode::disconnectCb, this, _1));
 }
 
-//=================================================================================================
 LaserScanKinectNode::~LaserScanKinectNode()
 {
     sub_.shutdown();
 }
 
-//=================================================================================================
 void LaserScanKinectNode::depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
                                   const sensor_msgs::CameraInfoConstPtr& info_msg)
 {
@@ -74,7 +36,6 @@ void LaserScanKinectNode::depthCb(const sensor_msgs::ImageConstPtr& depth_msg,
     }
 }
 
-//=================================================================================================
 void LaserScanKinectNode::connectCb(const ros::SingleSubscriberPublisher& pub)
 {
     std::lock_guard<std::mutex> lock(connect_mutex_);
@@ -87,7 +48,6 @@ void LaserScanKinectNode::connectCb(const ros::SingleSubscriberPublisher& pub)
     }
 }
 
-//=================================================================================================
 void LaserScanKinectNode::disconnectCb(const ros::SingleSubscriberPublisher& pub)
 {
     std::lock_guard<std::mutex> lock(connect_mutex_);
@@ -99,7 +59,6 @@ void LaserScanKinectNode::disconnectCb(const ros::SingleSubscriberPublisher& pub
     }
 }
 
-//=================================================================================================
 void LaserScanKinectNode::reconfigureCb(laserscan_kinect::LaserscanKinectConfig& config,
                                         uint32_t level)
 {
@@ -118,5 +77,4 @@ void LaserScanKinectNode::reconfigureCb(laserscan_kinect::LaserscanKinectConfig&
     converter_.setScanConfigurated(false);
 }
 
-//=================================================================================================
-} // end of namespace
+}
--- laserscan_kinect/src/laserscan_kinect_nodelet.cpp
+++ laserscan_kinect/src/laserscan_kinect_nodelet.cpp
@@ -1,41 +1,8 @@
-/******************************************************************************
- * Software License Agreement (BSD License)
- *
- * Copyright (c) 2016, Michal Drwiega (drwiega.michal@gmail.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in the
- *        documentation and/or other materials provided with the distribution.
- *     3. Neither the name of the copyright holder nor the names of its
- *        contributors may be used to endorse or promote products derived
- *        from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *****************************************************************************/
-/**
- * @file   laserscan_kinect_nodelet.cpp
- * @author Michal Drwiega (drwiega.michal@gmail.com)
- * @brief  laserscan_kinect package
- */
-
 #include <laserscan_kinect/laserscan_kinect_node.h>
-#include <nodelet/nodelet.h>
+
 #include <memory>
+
+#include <nodelet/nodelet.h>
 
 namespace laserscan_kinect
 {
@@ -43,19 +10,19 @@ namespace laserscan_kinect
 class LaserScanKinectNodelet : public nodelet::Nodelet
 {
 public:
-  LaserScanKinectNodelet()  {};
-  ~LaserScanKinectNodelet() {}
+  LaserScanKinectNodelet()  = default;
+  ~LaserScanKinectNodelet() = default;
 
 private:
   virtual void onInit()
   {
     converter.reset(new LaserScanKinectNode(getNodeHandle(), getPrivateNodeHandle()));
   };
-  
+
   std::shared_ptr<LaserScanKinectNode> converter;
 };
 
-} // end of namespace
+}
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_DECLARE_CLASS(laserscan_kinect, LaserScanKinectNodelet,laserscan_kinect::LaserScanKinectNodelet, nodelet::Nodelet);
