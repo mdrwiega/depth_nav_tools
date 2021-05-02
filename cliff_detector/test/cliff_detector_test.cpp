83fb65d58bf8287244b1ee130df79e5d9c89b06c
Michal Drwiega <drwiega.michal@gmail.com>
2021-05-02 16:57:47 +0200

    [cliff_detector] Tests migrated to ROS2

--- cliff_detector/test/cliff_detector_test.cpp
+++ cliff_detector/test/cliff_detector_test.cpp
@@ -1,13 +1,13 @@
 #include <cliff_detector/cliff_detector.h>
 
 #include <iostream>
-
+#include <sensor_msgs/image_encodings.hpp>
 #include <gtest/gtest.h>
 
 class CliffDetectorTest : public ::testing::Test {
  public:
-  sensor_msgs::ImagePtr depth_msg;
-  sensor_msgs::CameraInfoPtr info_msg;
+  sensor_msgs::msg::Image::SharedPtr depth_msg;
+  sensor_msgs::msg::CameraInfo::SharedPtr info_msg;
   cliff_detector::CliffDetector detector;
 
   unsigned img_height { 480 };
@@ -21,40 +21,38 @@ class CliffDetectorTest : public ::testing::Test {
     detector.setDepthImgStepCol(8);
     detector.setDepthImgStepRow(8);
     detector.setBlockSize(16);
-    detector.setRangeLimits(0.5, 5.0);
+    detector.setMinRange(0.5);
+    detector.setMaxRange(5.0);
     detector.setSensorMountHeight(0.5);
     detector.setSensorTiltAngle(10);
   }
 
   void setDefaultInfoMsg() {
-    info_msg.reset(new sensor_msgs::CameraInfo);
+    info_msg.reset(new sensor_msgs::msg::CameraInfo);
     info_msg->header.frame_id = "depth_frame";
-    info_msg->header.seq = 100;
     info_msg->height = img_height;
     info_msg->width = img_width;
     info_msg->distortion_model = "plumb_bob";
-    info_msg->D.resize(5);
-    info_msg->K[0] = 570;
-    info_msg->K[2] = 314;
-    info_msg->K[4] = 570;
-    info_msg->K[5] = 239;
-    info_msg->K[8] = 1.0;
-    info_msg->R[0] = 1.0;
-    info_msg->R[4] = 1.0;
-    info_msg->R[8] = 1.0;
-    info_msg->P[0] = 570;
-    info_msg->P[2] = 314;
-    info_msg->P[5] = 570;
-    info_msg->P[6] = 235;
-    info_msg->P[10] = 1.0;
+    info_msg->d.resize(5);
+    info_msg->k[0] = 570;
+    info_msg->k[2] = 314;
+    info_msg->k[4] = 570;
+    info_msg->k[5] = 239;
+    info_msg->k[8] = 1.0;
+    info_msg->r[0] = 1.0;
+    info_msg->r[4] = 1.0;
+    info_msg->r[8] = 1.0;
+    info_msg->p[0] = 570;
+    info_msg->p[2] = 314;
+    info_msg->p[5] = 570;
+    info_msg->p[6] = 235;
+    info_msg->p[10] = 1.0;
   }
 
   template<typename T>
   void setDefaultDepthMsg(T value) {
-    depth_msg.reset(new sensor_msgs::Image);
+    depth_msg.reset(new sensor_msgs::msg::Image);
     depth_msg->header.frame_id = "depth_frame";
-    depth_msg->header.seq = 100;
-    depth_msg->header.stamp.fromSec(10.0);
     depth_msg->height = img_height;
     depth_msg->width = img_width;
     depth_msg->is_bigendian = false;
