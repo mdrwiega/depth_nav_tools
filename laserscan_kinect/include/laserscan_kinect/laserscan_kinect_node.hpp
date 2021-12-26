03cc27e1eb2261eac5d51444bc7f50cb27238344
Michal Drwiega <drwiega.michal@gmail.com>
2021-12-26 02:57:50 +0100

    Switched license to Apache

--- laserscan_kinect/include/laserscan_kinect/laserscan_kinect_node.hpp
+++ laserscan_kinect/include/laserscan_kinect/laserscan_kinect_node.hpp
@@ -1,30 +1,16 @@
-// Software License Agreement (BSD License)
+// Copyright 2016-2021 Michał Drwięga (drwiega.michal@gmail.com)
 //
-// Copyright (c) 2016-2021, Michal Drwiega (drwiega.michal@gmail.com)
-// All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     1. Redistributions of source code must retain the above copyright
-//        notice, this list of conditions and the following disclaimer.
-//     2. Redistributions in binary form must reproduce the above copyright
-//        notice, this list of conditions and the following disclaimer in the
-//        documentation and/or other materials provided with the distribution.
-//     3. Neither the name of the copyright holder nor the names of its
-//        contributors may be used to endorse or promote products derived
-//        from this software without specific prior written permission.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef LASERSCAN_KINECT__LASERSCAN_KINECT_NODE_HPP_
 #define LASERSCAN_KINECT__LASERSCAN_KINECT_NODE_HPP_
@@ -53,15 +39,15 @@ public:
 
 private:
   /**
-   * @brief depthCb is a callback which is called when new depth image appear
+   * @brief It is a callback which is called when new depth image is received
    *
    * Callback for depth image and camera info.
-   * It converts depth image to laserscan and publishes it at the end.
+   * It converts depth image to laserscan and publishes it.
    *
    * @param image Depth image provided by image_transport.
    * @param info CameraInfo provided by image_transport.
    */
-  void depthCb(
+  void depthCallback(
     const sensor_msgs::msg::Image::ConstSharedPtr & image,
     const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info);
   /**
