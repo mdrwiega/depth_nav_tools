766f0a0d78aa98ba63c8501660fd8c1b54dd5c79
Omer Spalter <omers@nakairobotics.com>
2022-11-06 21:20:08 +0200

    Additional Humble updates

--- nav_layer_from_points/src/costmap_layer.cpp
+++ nav_layer_from_points/src/costmap_layer.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <nav_layer_from_points/costmap_layer.h>
+#include <nav_layer_from_points/costmap_layer.hpp>
 
 #include <fstream>
 
